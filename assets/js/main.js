2a83a51355e2be6dc5fbe7fd64bd630832af7432
DanielMaciel89 <damacie@outlook.com>
2022-05-04 23:09:34 -0300

    init

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll');
+  }
+  else {
+    navigation.classList.remove('scroll');
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home, 
+#home img, 
+#home .item, 
+#services, 
+#services. header, 
+#services .card,
+#about,
+#about .header,
+#about .content`);
